d105d47822b7fb8c547d7ae451b90f16f967cb4b
osgarc05 <osgarc05@ucm.es>
2022-11-22 17:03:34 +0100

    Mas movidas de transicion para coger el submarino

--- Mini Sub/Assets/scripts/playerSubmarine.cs
+++ Mini Sub/Assets/scripts/playerSubmarine.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class playerSubmarine : MonoBehaviour
 {
     private Transform selfTransform;
-    public subController submarine;
+    public GameObject submarine;
     public Vector3 posSit, posUp;
 
     public bool driving = false;
@@ -19,10 +19,13 @@ public class playerSubmarine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown("q"))
         {
             driving = !driving;
-            submarine.enabled = driving;
+            transform.Rotate(new Vector3(0, 1, 0), 180);
+            transform.parent.transform.Rotate(new Vector3(0, 1, 0), 180);
+            submarine.GetComponent<subController>().enabled = driving;
+            submarine.GetComponent<Rigidbody>().isKinematic = !driving;
             this.gameObject.GetComponent<playerController>().enabled = !driving;
             this.gameObject.GetComponent<CharacterController>().enabled = !driving;
             if (driving)
